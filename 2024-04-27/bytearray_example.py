a379b2ad372c5197d8d755b03e2dbd71ec9ba9ad
Dave Musicant (expanse) <dmusicant-expanse@carleton.edu>
2026-04-27 14:37:57 -0500

    Scripted auto-commit on change (2026-04-27 14:37:57) by gitwatch.sh

--- 2024-04-27/bytearray_example.py
+++ 2024-04-27/bytearray_example.py
@@ -1,6 +1,15 @@
+# struct is a library for manipulating
+# data in and out of bytes
+import struct
+
 mydata = bytearray(10)
 print(mydata)
 # Not a useful way of handling, but we'll try this
 mydata[0:3] = b"hey"
 print(mydata)
-mydata[0] = 12345
+#mydata[0] = 12345
+# pack_into(format ,  bytearray, offset, value)
+#format is a string indicating type of what's going
+# in there, which affects how many bytes are used
+print('A short is this many bytes', struct.calcsize('h')
+struct.pack_into('h',  mydata, 0, 12345)
