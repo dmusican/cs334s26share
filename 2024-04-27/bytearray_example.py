d91dc069fe8af1bbba83ecf1ef4a24e16d0d62b7
Dave Musicant (expanse) <dmusicant-expanse@carleton.edu>
2026-04-27 14:42:21 -0500

    Scripted auto-commit on change (2026-04-27 14:42:21) by gitwatch.sh

--- 2024-04-27/bytearray_example.py
+++ 2024-04-27/bytearray_example.py
@@ -26,5 +26,5 @@ val2 = struct.unpack_from('h', mydata, 2)
 print(val1)
 print(val2)
 
-multiple = struct.unpack_from('2h, mydata, 0)
+multiple = struct.unpack_from('2h', mydata, 0)
 print(multiple)
